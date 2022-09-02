fc2dd1603eefdd31f2860e3e1bbbdac1dc33643e
NMasanobu <nmkmsnb@gmail.com>
2022-09-03 00:43:58 +0900

    三目並べ改良

--- game/tic_tac_toe.py
+++ game/tic_tac_toe.py
@@ -4,16 +4,24 @@ class TicTacToe():
     def __init__(self, use_logger=False, player_first=None):
         self.use_logger = use_logger
         self.player_first = player_first
+        if player_first is not None:
+            self.fix_play_order = True
+        else:
+            self.fix_play_order = False
+
         self.reset()
 
     def reset(self):
+        self.started = False
         self.matrix = np.zeros((3, 3))
         self.available_actions = list(range(9))
-        if self.player_first is None:
-            self.player_first = np.random.randint() % 2 == 0
+        if not self.fix_play_order:
+            self.player_first = np.random.randint(2) % 2 == 0
         self.cpu = None
 
     def start(self):
+        self.started = True
+
         if not self.player_first:
             self.cpu_action()
 
@@ -23,10 +31,19 @@ class TicTacToe():
     def get_available_actions(self):
         return self.available_actions
 
+    def get_game_settings(self):
+        game_settings = {
+            'player_first': self.player_first
+        }
+
+        return game_settings
+
     def step(self, action):
         '''
         returns: state after cpu action, following player action
         '''
+        if not self.started:
+            raise Exception('Game has not started.')
         # player action
         coord = self.action2coord(action)
 
@@ -113,25 +130,67 @@ class TicTacToe():
 
         return (action // 3, action % 3)
 
-    def play(self):
-        done = False
-        while done:
-            done = True
-
 if __name__ == '__main__':
-    game = TicTacToe(use_logger=True, player_first=True)
+    game = TicTacToe()
+    
+    n_episodes = 1000
 
-    state = 0
-    while state == 0:
-        available_actions = game.get_available_actions()
-        action = np.random.choice(available_actions)
-        state = game.step(action)
+    # result
+    # 0: player first win
+    # 1: player first lose
+    # 2: player first draw
+    # 3: cpu first win
+    # 4: cpu first lose
+    # 5: cpu first draw
+    # 9: others
+    results = np.zeros(n_episodes)
 
-    if state == 1:
-        print('You win!')
-    elif state == 2:
-        print('You lose...')
-    elif state == 3:
-        print('Draw.')
-    elif state == 9:
-        print('No contest.')
+    for i in range(n_episodes):
+        game.reset()
+        game.start()
+        game_settings = game.get_game_settings()
+        player_first = game_settings['player_first']
+
+        state = 0
+        while state == 0:
+            available_actions = game.get_available_actions()
+            action = np.random.choice(available_actions)
+            state = game.step(action)
+
+        if state == 1:
+            if player_first:
+                results[i] = 0
+            else:
+                results[i] = 3
+
+        elif state == 2:
+            if player_first:
+                results[i] = 1
+            else:
+                results[i] = 4
+
+        elif state == 3:
+            if player_first:
+                results[i] = 2
+            else:
+                results[i] = 5
+
+        elif state == 9:
+            results[i] = 9
+            
+    n_player_first_win = (results==0).sum()
+    n_player_first_lose = (results==1).sum()
+    n_player_first_draw = (results==2).sum()
+    n_cpu_first_win = (results==3).sum()
+    n_cpu_first_lose = (results==4).sum()
+    n_cpu_first_draw = (results==5).sum()
+    others = (results==9).sum()
+
+    print('results:')
+    print(f'player first win: {n_player_first_win}')
+    print(f'player first lose: {n_player_first_lose}')
+    print(f'player first draw: {n_player_first_draw}')
+    print(f'cpu first win: {n_cpu_first_win}')
+    print(f'cpu first lose: {n_cpu_first_lose}')
+    print(f'cpu first draw: {n_cpu_first_draw}')
+    print(f'others: {others}')
